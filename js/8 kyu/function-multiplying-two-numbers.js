47dc09d4331f70d50798dfe0882b6d31628594f1
Alexander <holub.alexander.it@gmail.com>
2022-04-11 23:59:43 +0300

    feat: solved two problems (Function multiplying two numbers [8 kyu], Merge two sorted arrays into one [8 kyu])

---
+++ js/8 kyu/function-multiplying-two-numbers.js
@@ -0,0 +1,9 @@
+/* 
+  Function 3 - multiplying two numbers
+
+  Implement a function which multiplies two numbers.
+
+  Link: https://www.codewars.com/kata/523b66342d0c301ae400003b/train/javascript
+*/
+
+const multiply = (a, b) => a * b;
