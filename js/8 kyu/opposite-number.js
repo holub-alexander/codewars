f20bd7b7d93fc6deb6224050584a6cbeba6b1689
Alexander <holub.alexander.it@gmail.com>
2022-05-03 23:59:06 +0300

    feat: solved two problems (Disemvowel trolls [7 kyu], Opposite number [8 kyu])

---
+++ js/8 kyu/opposite-number.js
@@ -0,0 +1,17 @@
+/* 
+  Opposite number
+
+  Very simple, given an integer or a floating-point number, find its opposite.
+
+  Examples:
+  1: -1
+  14: -14
+  -34: 34
+
+  Link: https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
+*/
+
+const opposite = (number) => -number;
+
+console.log(opposite(1)); // 1
+console.log(opposite(0)); // 0
