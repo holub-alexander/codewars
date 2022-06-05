a4528bab003b8d3ebc7b6b44028712503c2c1f50
Alexander Holub <holub.alexander.it@gmail.com>
2022-06-05 22:26:42 +0300

    feat: solved two problems (Convert a number to a string [8 kyu], Square(n) sum [8 kyu])

---
+++ js/8 kyu/convert-a-number-to-a-string.js
@@ -0,0 +1,18 @@
+/* 
+  Convert a Number to a String!
+
+  We need a function that can transform a number into a string.
+  What ways of achieving this do you know?
+
+  Examples:
+  123 --> "123"
+  999 --> "999"
+
+  Link: https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+*/
+
+const numberToString = (num) => num.toString();
+
+console.log(numberToString(67)); // 67
+console.log(numberToString(123)); // 123
+console.log(numberToString(-91)); // -91
